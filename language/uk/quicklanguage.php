4920bb44868bc7f4f3e72a59bd1e28eec552209d
alg5 <chgk.globus@gmail.com>
2014-11-14 19:02:37 +0200

    travis8

--- language/uk/quicklanguage.php
+++ language/uk/quicklanguage.php
@@ -34,6 +34,6 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'QUICK_LANG_NO'	=> 'мовний пакет ISO =%s не встановлений', 
+	'QUICK_LANG_NO'	=> 'мовний пакет ISO =%s не встановлений',
 	'QUICK_LANG_EN_DISABLE'	=> 'швидка зміна на англійську мову заборонена адміністратором. ви можете змінити мову через особистий розділ.',
 ));
