05024e27d75ba2999553f1804e4318e67e7e641e
Raphaël <Galixte@users.noreply.github.com>
2015-03-12 10:19:27 +0100

    FRENCH translation for quicklanguage.php file
    FRENCH translation for quicklanguage.php file

---
+++ language/fr/quicklanguage.php
@@ -0,0 +1,45 @@
+<?php
+/** 
+*
+* quicklanguage [French]
+* French translation by Galixte (http://www.galixte.com)
+*
+* @package quicklanguage
+* @copyright (c) 2014 alg
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'QUICK_LANG_NO'	=> 'La langue ISO =%s n’est pas installée',
+	'QUICK_LANG_EN_DISABLE'	=> 'Le changement rapide de langue pour l’anglais est bloqué par l’administrateur. Vous pouvez réaliser cette action dans le panneau d’administration',
+	'FORUM_NAME_1'	=> 'Chugunok',
+	'FORUM_NAME_8'	=> 'Hyde Park',
+	'FORUM_NAME_29'	=> '13-th sector',
+	'FORUM_DESC_8'	=> 'Uncensored questions',
+));
