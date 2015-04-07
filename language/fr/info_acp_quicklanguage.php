e1a0db59860e5a15fe0b9060d24baa92cb71391d
Raphaël M. <Galixte@users.noreply.github.com>
2015-04-07 09:06:46 +0200

    Update of FRENCH translation for info_acp_quicklanguage.php file
    Update of FRENCH translation for info_acp_quicklanguage.php file

--- language/fr/info_acp_quicklanguage.php
+++ language/fr/info_acp_quicklanguage.php
@@ -43,13 +43,13 @@ $lang = array_merge($lang, array(
 	'ACP_QUICK_LANGUAGE_SETTINGS_COMMON'				=> 'Paramètre communs',
 	'ACP_QUICK_LANGUAGE_SETTINGS_SHOW'				=> 'Voir les paramètres',
 
-	'ACP_QUICK_LANGUAGE_IS_ENGLISH_SHOW'			=> 'Afficher l’anglais',
-	'ACP_QUICK_LANGUAGE_IS_ENGLISH_SHOW_EXPLAIN'	=> 'Par défaut une icône pour l’anglais sera affichée. <br />Pour les forums qui ne pratiquent pas l’anglais il est recommandé de désactiver cette option.',
+	'ACP_QUICK_LANGUAGE_IS_ENGLISH_SHOW'			=> 'Afficher les drapeaux',
+	'ACP_QUICK_LANGUAGE_IS_ENGLISH_SHOW_EXPLAIN'	=> 'Par défaut une icône (drapeau) pour l’anglais sera affichée. <br />Si vous avez plusieurs langues installées, vous aurez autant de drapeaux correspondants.',
 	'ACP_QUICK_LANGUAGE_ALLOW_GUESTS'			=> 'Permettre aux invités de changer de langue',
 	'ACP_QUICK_LANGUAGE_ALLOW_GUESTS_EXPLAIN'	=> 'Ce paramètre permet aux invités de changer aussi de langue. Tant qu’il ne sont pas connectés, un cookie sera utilisé pour mémoriser la langue sélectionnée.',
 	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SHOW'			=> 'Afficher le lien « Traduire »',
-	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SHOW_EXPLAIN'	=> 'Si activé, le lien va s’afficher en bas de chaque message. La traduction fonctionne au moyen de l’API du service Yandex, <br /> A l’avenir un service de traduction alternatif sera proposé.',
-	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SHOW_EXPLAIN_1'	=> 'Attention ! La traduction au moyen d’un service externe. Pour accéder à ce service, vous devez obtenir un clé pour l’API, la clé peut être obtenue gratuitement en cliquant sur le lien du service sélectionné. Si aucune clé n’est saisie, le lien ne s’affichera pas.',
+	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SHOW_EXPLAIN'	=> 'Si activé, le lien va s’afficher en bas de chaque message. La traduction fonctionne au moyen de l’API du service Yandex.<br />A l’avenir un service de traduction alternatif sera proposé.',
+	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SHOW_EXPLAIN_1'	=> 'Veuillez noter que la traduction fonctionne au moyen d’un service externe. Pour accéder à ce service vous devez obtenir un clé pour l’API. La clé peut être obtenue gratuitement en cliquant sur le lien du service sélectionné. Si aucune clé n’est saisie, le lien ne s’affichera pas.',
 	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_GET_KEY'			=> 'Obtenir la clé de l’API',
 	'ACP_QUICKLANGUAGE_IS_LINK_TRANSLATE_SET_KEY'			=> 'Saisir la clé de l’API',
 ));
